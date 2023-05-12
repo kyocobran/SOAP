2a57127ca2c3444a51e535f59d3f279147eb105d
kyocobran <kyocobran2000@yahoo.com>
2023-05-12 13:57:39 -0700

    SOAP 2023-05-12
    Initial Creation of the Visual Studio Program SOAP NOTE

---
+++ SOAP/SOAP_Form.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SOAP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Copy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(mainbox.SelectedText);
+        }
+
+        private void Paste_Click(object sender, EventArgs e)
+        {
+            mainbox.SelectedText = Clipboard.GetText();
+        }
+    }
+}
